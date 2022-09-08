54d380e07b1b87a40ecefc77a534bb5927b8d7c8
ErJump <fedelijunior@gmail.com>
2022-09-08 14:22:50 +0200

    feat: :lipstick: Adds style to header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,19 @@
 <header>
-    <h1>Header</h1>
+    <div class="container-lg py-3">
+        <div class="row justify-content-between">
+            <div class="col-2">
+                <img class="w-100" src="{{asset('images/logo.png')}}" alt="logo">
+            </div>
+            <div class="col-4">
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </header>
